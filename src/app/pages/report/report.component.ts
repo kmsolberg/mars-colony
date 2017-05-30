332ec775932d7e7b6505778fce752c623a639074
Kat Solberg <katsolberg@gmail.com>
2017-05-30 15:44:17 -0700

    get request for alien types, generate in dropdown menu on report page

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Aliens } from '../../models/aliens';
+import { AliensService } from '../../services/aliens.service';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
+  providers: [AliensService]
 })
 export class ReportComponent implements OnInit {
 
-  
+  aliens: Aliens[] = [];
 
-  constructor() { }
+  constructor(private aliensService: AliensService) {
+
+   }
 
   ngOnInit() {
-
+    this.aliensService.getData()
+    .subscribe((data) => {
+      this.aliens = data.aliens;
+    });
   }
 
 }
