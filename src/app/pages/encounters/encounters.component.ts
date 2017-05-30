0caf9a5ce62a08d9a895d9d24e001a634bfc7773
Kat Solberg <katsolberg@gmail.com>
2017-05-30 14:56:31 -0700

    make get request for encounters list, generate on page

--- src/app/pages/encounters/encounters.component.ts
+++ src/app/pages/encounters/encounters.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Encounter } from '../../models/encounter';
+import { EncountersService } from '../../services/encounters.service';
 
 @Component({
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
-  styleUrls: ['./encounters.component.scss']
+  styleUrls: ['./encounters.component.scss'],
+  providers: [EncountersService]
 })
 export class EncountersComponent implements OnInit {
-  message = 'Hello';
-  listofMessages = [
-    'Ciao',
-    'Bonjour',
-    'Buongiorno',
-    'Wassup'
-  ];
 
-  constructor() { }
+  encounters: Encounter[] = [];
 
+  constructor(private encounterService: EncountersService) { 
+
+  }
+  
   ngOnInit() {
+    this.encounterService.getData()
+    .subscribe((data) => {
+      this.encounters = data.encounters;
+    });
   }
 
 }
